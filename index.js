b389785db5ea99e737c5013983e9325a83b3e3e5
Irissf <iris.seijo@gmail.com>
2022-03-26 12:05:29 +0100

    play :D

--- index.js
+++ index.js
@@ -1,35 +1,20 @@
-
-//Cogemos el input
-const inputFile = document.querySelector(".input");
-
 //cogemos la lista para meter las canciones
 const lista = document.querySelector(".canciones--lista");
 
-let canciones;
-let cancion;
 
-inputFile.addEventListener("change", cargarArchivos);
 
-function cargarArchivos(e){
-    canciones = Array.from(e.target.files);
-    
-    canciones.forEach((cancion,index) =>{
-        const cancionTitulo = document.createElement("p");
-        cancionTitulo.textContent = cancion.name;
-        cancionTitulo.className = "cancion";
-        cancionTitulo.setAttribute("indice",index);
-        console.log(cancionTitulo.getAttribute("indice"));
-        cancionTitulo.addEventListener("click",reproducirCancion);
-        console.log(cancion.name);
+window.onload = ()=>{
+    //Cogemos el input
+    const file = document.querySelector(".input");
 
-        lista.appendChild(cancionTitulo);
-    });
-}
+    //el audio
+    const audio = document.querySelector(".audio");
 
-function reproducirCancion(e){
-    //cogemos el indice de la canción >.<
-    console.log(e.target.getAttribute("indice"));
-    const indice = e.target.getAttribute("indice");
-    cancion = canciones[indice];
-    console.log(cancion);
+    //vamos a detectar un cambio, al seleccionar una canción ocurre un cambio
+    //Otra manera de hacerlo seria con el file.addeventlistener('change',nombreFuncion)
+    file.onchange = function(){
+        const files = this.files;
+        //creamos un objeto de tipo url a partir del path que recibimos en files
+        audio.src = URL.createObjectURL(files[0])
+    }
 }
